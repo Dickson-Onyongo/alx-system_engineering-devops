920472f9e6d23b24e276c329e634a2013d7a24a1
Dickson-Onyongo <dicksononyong@gmail.com>
2024-07-02 01:08:41 +0300

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,9 +2,9 @@
 #Ensure the file is created with the specified attributes.
 
 file {'/tmp/school':
-  ensure => 'file',		#ensure the file is present
-  content => 'I love Puppet',	#content of the file.
-  mode => '0744',		#file permission.
-  owner => 'www-data',		#File owner
-  group => 'www-data',		#File group
+  ensure => 'file',  #ensure the file is present
+  content => 'I love Puppet',  #content of the file.
+  mode => '0744',  #file permission.
+  owner => 'www-data',  #File owner
+  group => 'www-data',  #File group
  }
