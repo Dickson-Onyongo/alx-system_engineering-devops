01e4878a748217763769f9fdcd16b87acba439b0
Dickson-Onyongo <dicksononyong@gmail.com>
2024-07-02 01:40:10 +0300

    configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-#Filename: create_a_file.pp
-#Ensure the file is created with the specified attributes.
+# This manifest creates a file at /tmp
 
-file {'/tmp/school':
-  ensure => 'file',  #ensure the file is present
-  content => 'I love Puppet',  #content of the file.
-  mode => '0744',  #file permission.
-  owner => 'www-data',  #File owner
-  group => 'www-data',  #File group
- }
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
